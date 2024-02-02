187a828e72481b134365881b8f873ac8bb8116d6
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-02-01 19:59:03 -0600

    Updating code

--- Thats-Just-Prime/Main.cpp
+++ Thats-Just-Prime/Main.cpp
@@ -2,10 +2,34 @@
 
 using namespace std;
 
+int FindLCD(int nmbr) {
+	int lcd = 0;
+
+	for (int i = 1; i < (nmbr / 2); i++) {
+		if ((nmbr % i) == 0)
+		{
+			return i;
+		}
+
+	}
+
+	return nmbr;
+
+}
+
+
+void PrimeTime(int nmbr) {
+
+	cout << "finding primes of " << nmbr;
+
+	
+
+}
+
 int main() {
+	int inpt = 10;
 
-
-
+	PrimeTime(inpt);
 
 	return 0;
 }
