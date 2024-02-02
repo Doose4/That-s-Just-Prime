8d17378f1a3c37661b07691a3d56e5f13b690dad
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-02-02 17:15:10 -0600

    Finished Main

--- Thats-Just-Prime/Main.cpp
+++ Thats-Just-Prime/Main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+vector<int> branches{};
 
 int FindLCD(int nmbr) {
 	int lcd = 0;
 
 
-	for (int i = 2; i < sqrt(nmbr); i++) {
+	for (int i = 2; i < nmbr; i++) {
 		if ((nmbr % i) == 0)
 		{
 			return i;
@@ -19,28 +23,42 @@ int FindLCD(int nmbr) {
 
 }
 
+void outpt(int nmbr) {
+	string str = to_string(branches[0]);
+	cout << endl << "the Primes of " << nmbr << " is:" << endl;
 
-int PrimeTime(int nmbr) {
+	for (int i = 1; i < branches.size(); i++) {
+		str = str + " X " + to_string(branches[i]);
+	}
+
+	cout << str;
+}
+
+void PrimeTime(int nmbr) {
+	//cout << nmbr << endl;
 	int lcd = (FindLCD(nmbr));
 
-	cout << "\nfinding primes of " << nmbr;
-
-	if (nmbr < 2){
-		return 1;
-	}
-	else if (lcd == 1) {
-		return nmbr;
-	}
-
-	cout << "\n" << lcd << " X " << PrimeTime(nmbr / lcd);
+	cout << "finding primes of " << nmbr << endl;
 	
+
+	if ((nmbr <= 2) || (lcd == 1)){
+		//cout << outpt;
+		branches.push_back(nmbr);
+		return;
+	}
+	
+	//cout << lcd << endl;
+	//cout << nmbr / lcd << endl << endl;
+	branches.push_back(lcd);
+	PrimeTime(nmbr / lcd);
 
 }
 
 int main() {
-	int inpt = 10;
+	int inpt = 20;
 
 	PrimeTime(inpt);
+	outpt(inpt);
 
 	return 0;
 }
